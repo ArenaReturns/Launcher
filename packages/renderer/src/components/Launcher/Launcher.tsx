0fda7380ce32fe5ca6c3936ad63ec20e5d1471d7
Jordan Rey <rey.jordan.1998@gmail.com>
2023-05-09 08:10:44 +0200

    feat: add a simple loader

--- packages/renderer/src/components/Launcher/Launcher.tsx
+++ packages/renderer/src/components/Launcher/Launcher.tsx
@@ -4,6 +4,8 @@ import LogoAndSocial from "../LogoAndSocial";
 import NewsCarousel from "../NewsCarousel";
 import TitleBar from "../TitleBar";
 import styles from "./Launcher.module.scss";
+import { Oval } from "react-loader-spinner";
+import { useState, useEffect } from "react";
 
 const enableDevMode = () => {
   window.api.ipc.send("enableDevMode");
@@ -14,6 +16,18 @@ const toggleDevTools = () => {
 };
 
 export const Launcher = () => {
+  const [launcherReady, setLauncherReady] = useState(false);
+
+  useEffect(() => {
+    window.api.ipc.on("setLauncherReady", (_event: Event, ready: boolean) => {
+      setLauncherReady(ready);
+    });
+
+    window.api.ipc.invoke("getLauncherReady").then((ready: boolean) => {
+      setLauncherReady(ready);
+    });
+  }, []);
+
   useKonami(enableDevMode, {
     code: [68, 69, 86, 77, 79, 68, 69],
   });
@@ -25,15 +39,34 @@ export const Launcher = () => {
   return (
     <div className={styles.Launcher}>
       <TitleBar />
-      <div className={styles.container}>
-        <div className={styles.topPart}>
-          <NewsCarousel />
-          <LogoAndSocial />
+      {!launcherReady && (
+        <div className={styles.loader}>
+          <Oval
+            height={"15em"}
+            width={"15em"}
+            color="#D8C217"
+            wrapperStyle={{}}
+            wrapperClass=""
+            visible={true}
+            ariaLabel="oval-loading"
+            secondaryColor="#D8C217"
+            strokeWidth={2}
+            strokeWidthSecondary={2}
+          />
+          <div className={styles.loaderText}>Chargement de l'Hormonde...</div>
         </div>
-        <div className={styles.bottomPart}>
-          <Loader />
+      )}
+      {launcherReady && (
+        <div className={styles.container}>
+          <div className={styles.topPart}>
+            <NewsCarousel />
+            <LogoAndSocial />
+          </div>
+          <div className={styles.bottomPart}>
+            <Loader />
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
